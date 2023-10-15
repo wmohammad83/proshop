1e33e6d94cc68bc64298e99ad921e42ce0c10636
Waseem Mohammad <wmohammad83@icloud.com>
2023-10-15 20:15:50 +0100

    Created controller for products

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ dotenv.config();
 
 // IMPORT DB
 import connectDB from "./config/db.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import productRoutes from "./routes/productRoutes.js";
 const port = process.env.PORT || 5002;
 
@@ -16,5 +17,8 @@ app.get("/", (req, res) => {
 })
 
 app.use("/api/products", productRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server running on ${port}`))
